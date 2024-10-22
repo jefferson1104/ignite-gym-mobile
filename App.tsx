58ded5ef8c3636e4e8b3abe10be46b5929719fef
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2024-10-22 00:40:11 -0300

    chore: initial project structure

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import { Text, View, StatusBar } from 'react-native';
+import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
+
+export default function App() {
+  // Hooks
+  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
+
+
+  // Renders
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#202024' }}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      {fontsLoaded
+        ? <Text>Home</Text>
+        : <View />
+      }
+    </View>
+  );
+}
