131c68d0ab1f95cce930a6d7b775eee9c73752e3
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2025-01-15 10:15:42 -0300

    implementation to load exercises groups

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,16 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FlatList } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { Heading, HStack, VStack, Text } from "@gluestack-ui/themed";
+import { Heading, HStack, VStack, Text, useToast } from "@gluestack-ui/themed";
+
+import { api } from "@services/api";
 
 import { AppNavigatorRoutesProps } from "@routes/app.routes";
+
+import { AppError } from "@utils/app-error";
 
 import { HomeHeader } from "@components/HomeHeader";
 import { Group } from "@components/Group";
 import { ExerciseCard } from "@components/ExerciseCard";
+import { ToastMessage } from "@components/ToastMessage";
 
 export function Home() {
   // Hooks
+  const toast = useToast();
   const navigation = useNavigation<AppNavigatorRoutesProps>();
 
   // States
@@ -21,17 +27,49 @@ export function Home() {
     "Single-leg row",
     "Deadlift",
   ]);
-  const [groups, setGroups] = useState([
-    "Back",
-    "Biceps",
-    "Triceps",
-    "Shoulder",
-  ]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   // Methods
   function handleOpenExerciseDetails() {
     navigation.navigate("exercise");
   }
+
+  async function fetchGroups() {
+    try {
+      const response = await api.get("/groups");
+
+      if (response.data) {
+        setGroups(response.data);
+      }
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+
+      const title = isAppError
+        ? error.message
+        : "It was not possible to load groups";
+
+      const toastId = "load-groups-error";
+
+      toast.show({
+        placement: "top",
+        render: () => (
+          <ToastMessage
+            id={toastId}
+            title={title}
+            action="error"
+            onClose={() => toast.close(toastId)}
+          />
+        ),
+      });
+
+      console.error("Error to load groups: ", error);
+    }
+  }
+
+  // Effects
+  useEffect(() => {
+    fetchGroups();
+  }, []);
 
   // Renders
   return (
